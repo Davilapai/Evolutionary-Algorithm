f5f0451a3676606aa359e760f42e6c25e8f9e568
Dreamerend3008 <79518618+Dreamerend3008@users.noreply.github.com>
2026-09-16 19:32:55 -0500

    algorithm completed

---
+++ genetic_algorithm/Individual.py
@@ -0,0 +1,34 @@
+import random
+import numpy as np
+
+
+class Individual:
+    def __init__(self, state: list[int]):
+        self.n = len(state)
+        self.state = np.array(state)
+
+    def fitness(self) -> int:
+        """
+        Returns evaluation of the current state of the individual
+        """
+        return 0
+        """
+        fit(x) = 
+        """
+    def mutate(self, mutation_factor: float):
+        for i in range(self.n):
+            if random.random() < mutation_factor:
+                x = random.randint(0, self.n-1)
+                self.state[i],self.state[x] = self.state[x],self.state[i]
+        return self
+
+    @staticmethod
+    def random_Individual_legal(n: int):
+        v = [n for n in range(n)]
+        v = random.sample(v, n)
+        return Individual(v)
+
+    @staticmethod
+    def random_Individual_ilegal(n: int):
+        v = [random.randint(1, n) for _ in range(n)]
+        return Individual(v)
